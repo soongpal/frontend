79d18583f6cf824da470e7aabcc7ef171e54724f
Garam100 <backgaram123@gmail.com>
2025-07-06 11:43:09 +0900

    refactor: navbar하나로 통합하고 row, col로 그리드 형식으로 배치, banner 크기 변경, feat: navbar상단 고정

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,46 +1,56 @@
 import type React from "react";
-import { Container, Nav, Navbar, InputGroup, FormControl, Button } from "react-bootstrap";
+import { Container, Nav, Navbar, InputGroup, FormControl, Button, Row, Col } from "react-bootstrap";
 import { Search, List } from 'react-bootstrap-icons';
 
 const Header : React.FC = () =>{
 
     return(
         <div>
-            <Navbar sticky="top" bg="light" data-bs-theme="light">
-            <Container justify-content-between>
-                <Navbar.Brand href="/"><h1>숭팔이로고</h1></Navbar.Brand>
-                <Nav>
-                    <Nav.Link href="/signup">회원가입</Nav.Link>
-                    <Nav.Link href="/login">로그인</Nav.Link>
-                    <Nav.Link href="/mypage">마이페이지</Nav.Link>
-                </Nav>
-            </Container>
-            </Navbar>
+            <Navbar fixed="top" bg="light" data-bs-theme="light" className="flex-column">
+                <Container>
+                    <Row className="w-100 align-items-center">
+                        <Col className="d-flex justify-content-start">
+                            <Navbar.Brand href="/"><h1>숭팔이로고</h1></Navbar.Brand>
+                        </Col>
+                        
+                        <Col className="d-flex justify-content-end">
+                            <Nav>
+                                <Nav.Link href="/signup">회원가입</Nav.Link>
+                                <Nav.Link href="/login">로그인</Nav.Link>
+                                <Nav.Link href="/mypage">마이페이지</Nav.Link>
+                            </Nav>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row className="w-100 align-items-center">
+                        <Col className="d-flex justify-content-start">
+                            <Nav className="d-flex align-items-center">
+                                <List size={24} />
+                                <Nav.Link href="/grouppurchase">공동구매</Nav.Link>
+                                <Nav.Link href="/usedtrade">중고거래</Nav.Link>
+                            </Nav>
+                        </Col>
 
-            <Navbar sticky="top" bg="light" data-bs-theme="light">
-            <Container justify-content-between>
-                 <Nav className="d-flex align-items-center">
-                    <List size={24} />
-                    <Nav.Link href="/grouppurchase">공동구매</Nav.Link>
-                    <Nav.Link href="/usedtrade">중고거래</Nav.Link>
-                </Nav>
-
-                <InputGroup className="w-50">
-                    <FormControl
-                        placeholder="상품명 입력"
-                        aria-label="Search"
-                        className="border-0 shadow-none"
-                        style={{ backgroundColor: 'white' }}
-                    />
-                    <Button 
-                    variant="outline-secondary"
-                    className="border-0"
-                    style={{ backgroundColor: 'white' }}
-                    >
-                        <Search />
-                    </Button>
-                </InputGroup>
-            </Container>
+                        <Col className="d-flex justify-content-end">
+                            <InputGroup className="w-50">
+                                <FormControl
+                                    placeholder="상품명 입력"
+                                    aria-label="Search"
+                                    className="border-0 shadow-none"
+                                    style={{ backgroundColor: 'white' }}
+                                />
+                                <Button 
+                                variant="outline-secondary"
+                                className="border-0"
+                                style={{ backgroundColor: 'white' }}
+                                >
+                                    <Search />
+                                </Button>
+                            </InputGroup>
+                        </Col>
+                    </Row>
+                </Container>
             </Navbar>
         </div>
     );
