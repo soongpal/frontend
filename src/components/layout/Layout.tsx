4141cf9dc35d1de6033573a189b56afdac54c098
Garam100 <backgaram123@gmail.com>
2025-07-08 15:21:09 +0900

    feat: layout페이지 완성

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,1 +1,20 @@
 // Header, Footer, Outlet을 포함하는 메인 레이아웃
+
+import type React from "react";
+import Header from "./header/Header";
+import { Outlet } from "react-bootstrap-icons";
+import Footer from "./footer/Footer";
+
+const Layout: React.FC = () =>{
+    return(
+        <div className="App">
+            <Header></Header>
+            <main className="content">
+                <Outlet></Outlet>
+            </main>
+            <Footer></Footer>
+        </div>
+    );
+}
+
+export default Layout;
