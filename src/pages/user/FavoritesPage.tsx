27d26aab7be5bc9d19f1d8f489ca86bd729393cb
Garam100 <backgaram123@gmail.com>
2025-08-02 16:30:41 +0900

    feat: 좋아요 페이지 zustand로 수정

--- src/pages/user/FavoritesPage.tsx
+++ src/pages/user/FavoritesPage.tsx
@@ -1,27 +1,24 @@
 //좋아요 목록 페이지
-import dummy from "../../data/dummy.json"
 import type React from "react";
 import Filter from "../../components/common/Filter";
-import { useEffect, useState } from "react";
-import type { Product, Status } from "../../types/product";
+import { useState } from "react";
+import type { Status } from "../../types/product";
 import ProductGrid from "../../components/product/ProductGrid";
+import { useProductStore } from "../../stores/productSotre";
 
 const  FavoritesPage: React.FC = () =>{
-    //불러올 제품 목록
-    const [products, setProducts] = useState<Product[]>([]);
-
-    //필터 기능
+   // 처음 불러오기
+    const { products } = useProductStore();
+   
+    //필터 영역
     const [filter, setFilter] = useState<Status | null>(null);
-    
+   
     const handleFilter = (filter:Status | null)=>{
-         setFilter(filter);
+        setFilter(filter);
     };
 
     //제품 목록 불러오기
-    useEffect(() => {
-        const allProducts: Product[] = dummy as Product[];
-
-        const likedProducts = allProducts.filter((product) => {
+    const likedProducts = products.filter((product) => {
         if (filter === null) {
             return product.liked === true;
         }
@@ -29,16 +26,13 @@ const  FavoritesPage: React.FC = () =>{
             product.liked === true &&
             product.status === filter
         );
-        });
-
-        setProducts(likedProducts);
-    }, [filter]);
+    });
 
     return(
         <div className="container">
             <h3 className="text-center mt-3 mb-3"><b>관심 목록</b></h3>
             <Filter onFilterSelect = {handleFilter}></Filter>
-            <ProductGrid products={products}></ProductGrid>
+            <ProductGrid products={likedProducts}></ProductGrid>
 
         </div>
     )
