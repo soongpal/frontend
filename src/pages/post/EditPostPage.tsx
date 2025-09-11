7a332d0a670fb9376ada171cc84d06a5128965d0
Garam100 <backgaram123@gmail.com>
2025-09-11 14:27:32 +0900

    style: 검색 결과 없음 수정

--- src/pages/post/EditPostPage.tsx
+++ src/pages/post/EditPostPage.tsx
@@ -124,6 +124,7 @@ const EditPostPage: React.FC = () => {
                                 setUploadFiles={setNewImages}
                                 onFilesChange={handleImagesChange} 
             />
+            <p style={{ color: 'var(--soongpal-color)' }}>*JPG, JEPG, PNG 형식</p>
         </div>
                 
                 <div style={{ marginBottom: '1rem' }}>
