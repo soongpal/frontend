efb45a885ccdfcbed5b80598f98bfe659947ca18
Garam100 <backgaram123@gmail.com>
2025-09-20 21:46:57 +0900

    feat: 라우터 변경

--- src/routes/ChatRouter.tsx
+++ src/routes/ChatRouter.tsx
@@ -1,13 +1,18 @@
 //채팅 관련 라우트
 import { Route, Routes } from "react-router-dom";
-import ChatListPage from "../pages/chat/ChatListPage";
 import ChatRoomPage from "../pages/chat/ChatRoomPage";
+// ✨ 1. 새로 만든 레이아웃과 플레이스홀더 컴포넌트를 import 합니다.
+import ChatPageLayout from "../pages/chat/ChatPageLayout";
+import ChatPlaceholder from "../components/chat/ChatPlaceholder";
 
-const ChatRouter : React.FC = () => {
-    return(
+const ChatRouter: React.FC = () => {
+    return (
         <Routes>
-            <Route path="chatlist" element={<ChatListPage />}/>
-            <Route path="chatroom/:ChatId" element={<ChatRoomPage />}/>
+            <Route path="/" element={<ChatPageLayout />}>
+                <Route index element={<ChatPlaceholder />} />
+                <Route path=":ChatId" element={<ChatRoomPage />} />
+
+            </Route>
         </Routes>
     )
 }
