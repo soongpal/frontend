74e351c9441851915f9cdf27408981a55d017663
Garam100 <backgaram123@gmail.com>
2025-10-08 22:16:11 +0900

    feat: 테스트용 알림 받기 설정

---
+++ src/components/common/Notification.tsx
@@ -0,0 +1,30 @@
+//알림 권한 허용인지
+import React from 'react';
+import { requestPermissionAndGetToken } from '../../firebase';
+import { sendFcmToken } from '../../api/authAPI';
+import { BellFill } from 'react-bootstrap-icons';
+
+const NotificationButton: React.FC = () => {
+
+  const handleNotificationClick = async () => {
+    const token = await requestPermissionAndGetToken();
+
+    if (token) {
+      console.log("발급 토큰:", token);
+      try {
+        await sendFcmToken(token);
+        alert('알림을 허용하였습니다');
+      } catch (error) {
+        alert('알림 설정 중 오류 발생');
+      }
+    }
+  };
+
+  return (
+    <button onClick={handleNotificationClick}>
+      <BellFill/>
+    </button>
+  );
+};
+
+export default NotificationButton;
--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -8,6 +8,7 @@ import { PersonCircle, PersonFill, Search } from 'react-bootstrap-icons';
 import "../../../styles/Header.css";
 //store
 import { useAuthStore } from '../../../stores/UserStore';
+import NotificationButton from "../../common/Notification";
 
 const Header : React.FC = () =>{
 
@@ -38,10 +39,16 @@ const Header : React.FC = () =>{
                     
                         <Col>
                             {isLogin ? (
-                                //로그인일때->마이페이지
-                                <a href="/user/mypage" className="d-flex  justify-content-end align-items-center text-decoration-none text-dark">
-                                    <PersonCircle className="me-2"/>마이페이지
-                                </a>
+                                //로그인일때->마이페이지, 알림
+                                <div>
+                                    <NotificationButton/>
+                                    
+                                    <a href="/user/mypage" className="d-flex  justify-content-end align-items-center text-decoration-none text-dark">
+                                        <PersonCircle className="me-2"/>마이페이지
+                                    </a>
+                                    
+                                </div>
+                                
                                 ) : (
                                 //로그아웃일때->로그인/회원가입
                                 <a href="/auth/login" className="d-flex  justify-content-end align-items-center text-decoration-none text-dark">
